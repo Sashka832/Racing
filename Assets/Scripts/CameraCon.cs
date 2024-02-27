7810a16ae91119d2bb6a8262386bd85a21976fa4
Ruzeon <72289955+Ruzeon@users.noreply.github.com>
2024-02-27 16:12:24 +0900

    addition

---
+++ Assets/Scripts/CameraCon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCon : MonoBehaviour
+{
+    public Transform camTarget;
+    public float pLerp = .02f;
+    public float rLerp = .01f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, camTarget.position, pLerp);
+        transform.rotation = Quaternion.Lerp(transform.rotation, camTarget.rotation, rLerp);
+    }
+}
